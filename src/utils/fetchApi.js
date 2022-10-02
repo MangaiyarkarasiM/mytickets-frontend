72fc71ba29a0946f398428de898d1aaecfaff62c
MangaiyarkarasiM <mangaivmm@gmail.com>
2022-10-02 23:42:02 +0530

    url updated

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetchApi = axios.create({
-  baseURL: 'https://mytickets-backend.herokuapp.com/',
+  baseURL: 'https://mytickets-backend.herokuapp.com',
   //baseURL:'http://localhost:8000',
 })
 
