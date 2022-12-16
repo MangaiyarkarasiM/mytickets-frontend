10520a3ef55094bbc9cb8fdd2d21ce68cb44a9c7
MangaiyarkarasiM <mangaivmm@gmail.com>
2022-12-17 00:06:46 +0530

    endpoint update

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetchApi = axios.create({
-  baseURL: 'https://mytickets-backend.herokuapp.com',
+  baseURL: 'https://mytickets.vercel.app',
   //baseURL:'http://localhost:8000',
 })
 
